883b921ab9b171473526752f4c61edd459b3c628
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-04-13 18:00:28 +0100

    move code to new repo

---
+++ src/PlatformKit.Extensions.OperatingSystem/Extensions/IsVersionAtLeast/IsMacOSVersionAtLeastExtension.cs
@@ -0,0 +1,54 @@
+﻿/*
+        MIT License
+       
+       Copyright (c) 2024 Alastair Lundy
+       
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+       
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+       
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+using System;
+
+using AlastairLundy.System.Extensions.VersionExtensions;
+
+namespace PlatformKit.Extensions.OperatingSystem
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IsMacOSVersionAtLeastExtension
+    {
+        /// <summary>
+        /// Checks to see whether the specified version of macOS is the same or newer than the installed version of Windows.
+        /// </summary>
+        /// <param name="major"></param>
+        /// <param name="minor"></param>
+        /// <param name="build"></param>
+        /// <param name="revision"></param>
+        /// <returns></returns>
+        /// <exception cref="PlatformNotSupportedException"></exception>
+        // ReSharper disable once InconsistentNaming
+        public static bool IsMacOSVersionAtLeast(int major, int minor = 0, int build = 0, int revision = 0)
+        {
+            if (IsMacOSExtension.IsMacOS())
+            {
+                return GetSystemExtension.GetSystem(PlatformID.MacOSX).Version.IsAtLeast(new Version(major, minor, build, revision));
+            }
+
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
---
+++ src/PlatformKit.Extensions.OperatingSystem/Extensions/IsVersionAtLeast/IsWindowsVersionAtLeastExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AlastairLundy.System.Extensions.VersionExtensions;
+
+using PlatformKit.Extensions.OperatingSystem;
+
+namespace PlatformKit.Extensions.OperatingSystem
+{
+    public static class IsWindowsVersionAtLeastExtension
+    {
+        /// <summary>
+        /// Checks to see whether the specified version of Windows is the same or newer than the installed version of Windows.
+        /// </summary>
+        /// <param name="major"></param>
+        /// <param name="minor"></param>
+        /// <param name="build"></param>
+        /// <param name="revision"></param>
+        /// <returns></returns>
+        /// <exception cref="PlatformNotSupportedException"></exception>
+        public static bool IsWindowsVersionAtLeast(int major, int minor = 0, int build = 0, int revision = 0)
+        {
+            if (IsWindowsExtension.IsWindows())
+            {
+                return GetSystemExtension.GetSystem(PlatformID.Win32NT).Version.IsAtLeast(new Version(major, minor, build, revision));
+            }
+
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
