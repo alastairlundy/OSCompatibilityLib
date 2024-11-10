949410569a575cfa4a688be38d5aeecceedd65e2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-11-10 18:27:16 +0000

    clarify Runtime Identifier types

--- AlastairLundy.Extensions.Runtime/Identification/Enums/RuntimeIdentifierType.cs
+++ AlastairLundy.Extensions.Runtime/Identification/Enums/RuntimeIdentifierType.cs
@@ -29,8 +29,17 @@ namespace AlastairLundy.Extensions.Runtime.Identification
     /// </summary>
     public enum RuntimeIdentifierType
     {
+        /// <summary>
+        /// A Runtime Identifier that is valid for all architectures of an operating system.
+        /// </summary>
         AnyGeneric,
+        /// <summary>
+        /// A Runtime Identifier that is valid for all supported versions of the OS being run.
+        /// </summary>
         Generic,
+        /// <summary>
+        /// A Runtime Identifier that is valid for the specified OS and specified OS version being run.
+        /// </summary>
         Specific,
         /// <summary>
         /// This is meant for Linux use only. DO NOT USE ON WINDOWS or MAC.
