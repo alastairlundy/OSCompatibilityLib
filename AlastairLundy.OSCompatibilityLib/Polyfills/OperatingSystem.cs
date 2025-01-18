5716e45be874e46fc798ecaa4187d92fdf1f3ed0
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-01-18 16:18:32 +0000

    clarify polyfill code comments come from .NET API reference docs

--- AlastairLundy.OSCompatibilityLib/Polyfills/OperatingSystem.cs
+++ AlastairLundy.OSCompatibilityLib/Polyfills/OperatingSystem.cs
@@ -20,6 +20,8 @@
        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
        SOFTWARE.
+       
+       Code comments licensed from the .NET API Reference docs under CC BY 4.0 license. See THIRD_PARTY_NOTICES for more details.
    */
 
 using System;
